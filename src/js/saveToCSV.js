99d577db4a6373f3de5c00fdad59afc6768badde
Ertan Taner <62712555+Ertan222@users.noreply.github.com>
2023-03-28 02:21:42 +0300

    feat: add csv and file winner picker (#23)

---
+++ src/js/saveToCSV.js
@@ -0,0 +1,16 @@
+import fs from "fs";
+const saveToCSV = async (winnersOfMonth, type) => {
+  const file = "winners.csv";
+  const dirPath = "winners";
+  const fullPath = `${dirPath}/${file}`;
+  const csvHeaders = "name,date,type";
+  if(!fs.existsSync(dirPath)) {
+    await fs.promises.mkdir(dirPath);
+    await fs.promises.writeFile(`${fullPath}`, csvHeaders);
+  }
+  const currentData = (new Date()).toLocaleDateString();
+  winnersOfMonth.map(async (winner) =>  
+  await fs.promises.writeFile(`${fullPath}`, `\n${winner},${currentData},${type}`, {flag: "a"}))
+}
+
+export default saveToCSV;
